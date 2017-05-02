f0b50aeaa9e088ec9c9e9418e4cea25ec200da59
Jeremy Stucki <jeremy@interactivethings.com>
2017-05-02 15:29:18 +0200

    Update to latest catalog

--- catalog/index.js
+++ catalog/index.js
@@ -1,4 +1,6 @@
-import {render} from 'catalog';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Catalog, ContentLoader } from "catalog";
 const publicPath = process.env.PUBLIC_URL;
 
 // We know that Catalog uses react-router, and furthermore that it
@@ -7,131 +9,141 @@ const publicPath = process.env.PUBLIC_URL;
 // page views.
 //
 // Note that 'location.pathname' includes the PUBLIC_URL prefix!
-import {browserHistory} from 'react-router';
-browserHistory.listen((location) => {
-  if (typeof _paq !== 'undefined') {
-    _paq.push(['setCustomUrl', location.pathname]);
-    _paq.push(['trackPageView']);
+import { browserHistory } from "react-router";
+browserHistory.listen(location => {
+  if (typeof _paq !== "undefined") {
+    _paq.push(["setCustomUrl", location.pathname]);
+    _paq.push(["trackPageView"]);
   }
 });
 
-render(
+const pages = [
   {
-    title: 'Catalog',
-    useBrowserHistory: true,
-    basePath: publicPath,
-    logoSrc: publicPath + '/assets/catalog_logo.svg',
-    theme: {
-      // Uses default theme
-    },
+    path: "/",
+    title: "Introduction",
+    content: ContentLoader(() => import("../public/basics/get-started.md"))
+  },
+  {
+    path: "get-started",
+    title: "Get Started",
+    content: ContentLoader(() => import("../public/basics/get-started.md"))
+  },
+  {
+    path: "write-documentation",
+    title: "Write Documentation",
+    content: ContentLoader(() => import("../public/basics/markdown.md"))
+  },
+  {
+    path: "configuration",
+    title: "Configuration",
+    content: ContentLoader(() => import("../public/basics/configuration.md"))
+  },
+  {
+    path: "react-integration",
+    title: "React Integration",
+    content: ContentLoader(() => import("../public/react-integration.md"))
+  },
+  // {title: 'Basics', pages: [
+  // ]},
+  // {title: 'Advanced', pages: [
+  //   {path: 'advanced/configuration', title: 'Configuration', content: ContentLoader(() => import('../public/coming-soon.md'},))
+  //   {path: 'advanced/extending', title: 'Writing Specimens', content: ContentLoader(() => import('../public/coming-soon.md'},))
+  // ]},
+  {
+    title: "Specimens",
     pages: [
-      {path: '/', title: 'Introduction', src: publicPath + '/intro.md'},
       {
-        path: 'get-started',
-        title: 'Get Started',
-        src: publicPath + '/basics/get-started.md'
+        path: "specimens",
+        title: "Overview",
+        content: ContentLoader(() => import("../public/specimens/overview.md"))
       },
       {
-        path: 'write-documentation',
-        title: 'Write Documentation',
-        src: publicPath + '/basics/markdown.md'
+        path: "specimens/audio",
+        title: "Audio",
+        content: ContentLoader(() => import("../public/specimens/audio.md"))
       },
       {
-        path: 'configuration',
-        title: 'Configuration',
-        src: publicPath + '/basics/configuration.md'
+        path: "specimens/code",
+        title: "Code",
+        content: ContentLoader(() => import("../public/specimens/code.md"))
       },
       {
-        path: 'react-integration',
-        title: 'React Integration',
-        src: publicPath + '/react-integration.md'
-      },
-      // {title: 'Basics', pages: [
-      // ]},
-      // {title: 'Advanced', pages: [
-      //   {path: 'advanced/configuration', title: 'Configuration', src: publicPath + '/coming-soon.md'},
-      //   {path: 'advanced/extending', title: 'Writing Specimens', src: publicPath + '/coming-soon.md'},
-      // ]},
-      {
-        title: 'Specimens',
-        pages: [
-          {
-            path: 'specimens',
-            title: 'Overview',
-            src: publicPath + '/specimens/overview.md'
-          },
-          {
-            path: 'specimens/audio',
-            title: 'Audio',
-            src: publicPath + '/specimens/audio.md'
-          },
-          {
-            path: 'specimens/code',
-            title: 'Code',
-            src: publicPath + '/specimens/code.md'
-          },
-          {
-            path: 'specimens/color',
-            title: 'Color',
-            src: publicPath + '/specimens/color.md'
-          },
-          {
-            path: 'specimens/color-palette',
-            title: 'Color Palette',
-            src: publicPath + '/specimens/color-palette.md'
-          },
-          {
-            path: 'specimens/download',
-            title: 'Download',
-            src: publicPath + '/specimens/download.md'
-          },
-          {
-            path: 'specimens/hint',
-            title: 'Hint',
-            src: publicPath + '/specimens/hint.md'
-          },
-          {
-            path: 'specimens/html',
-            title: 'HTML',
-            src: publicPath + '/specimens/html.md',
-            styles: ['/example-style.css']
-          },
-          {
-            path: 'specimens/image',
-            title: 'Image',
-            src: publicPath + '/specimens/image.md'
-          },
-          {
-            path: 'specimens/table',
-            title: 'Table',
-            src: publicPath + '/specimens/table.md'
-          },
-          {
-            path: 'specimens/type',
-            title: 'Type',
-            src: publicPath + '/specimens/type.md'
-          },
-          {
-            path: 'specimens/video',
-            title: 'Video',
-            src: publicPath + '/specimens/video.md'
-          },
-          {
-            path: 'specimens/react',
-            title: 'React',
-            src: publicPath + '/specimens/react.md'
-          }
-        ]
+        path: "specimens/color",
+        title: "Color",
+        content: ContentLoader(() => import("../public/specimens/color.md"))
       },
       {
-        path: 'test',
-        title: 'Style Test',
-        src: publicPath + '/test/test.md',
-        styles: [publicPath + '/test/test.css'],
-        scripts: [publicPath + '/test/test.js'],
-        hideFromMenu: true
+        path: "specimens/color-palette",
+        title: "Color Palette",
+        content: ContentLoader(() =>
+          import("../public/specimens/color-palette.md")
+        )
+      },
+      {
+        path: "specimens/download",
+        title: "Download",
+        content: ContentLoader(() => import("../public/specimens/download.md"))
+      },
+      {
+        path: "specimens/hint",
+        title: "Hint",
+        content: ContentLoader(() => import("../public/specimens/hint.md"))
+      },
+      {
+        path: "specimens/html",
+        title: "HTML",
+        content: ContentLoader(() => import("../public/specimens/html.md")),
+        styles: ["/example-style.css"]
+      },
+      {
+        path: "specimens/image",
+        title: "Image",
+        content: ContentLoader(() => import("../public/specimens/image.md"))
+      },
+      {
+        path: "specimens/table",
+        title: "Table",
+        content: ContentLoader(() => import("../public/specimens/table.md"))
+      },
+      {
+        path: "specimens/type",
+        title: "Type",
+        content: ContentLoader(() => import("../public/specimens/type.md"))
+      },
+      {
+        path: "specimens/video",
+        title: "Video",
+        content: ContentLoader(() => import("../public/specimens/video.md"))
+      },
+      {
+        path: "specimens/react",
+        title: "React",
+        content: ContentLoader(() => import("../public/specimens/react.md"))
       }
     ]
   },
-  document.getElementById('catalog')
+  {
+    path: "test",
+    title: "Style Test",
+    content: ContentLoader(() =>
+      import(/* webpackChunkName: "test" */ "../public/test/test.md")
+    ),
+    styles: [publicPath + "/test/test.css"],
+    scripts: [publicPath + "/test/test.js"],
+    hideFromMenu: true
+  }
+];
+
+ReactDOM.render(
+  <Catalog
+    title="Catalog"
+    useBrowserHistory
+    basePath={publicPath}
+    logoSrc={publicPath + "/assets/catalog_logo.svg"}
+    theme={{
+      // Uses default theme
+    }}
+    pages={pages}
+  />,
+  document.getElementById("catalog")
 );
